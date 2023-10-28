44f123791fc98b6e2e6387c92c3c4d74f2aa7231
Le Quy Nhat <lequynhat1999@gmail.com>
2023-10-28 20:44:55 +0700

    commit code deploy

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld>
+    <template #header>
+      <div>lqnhat header</div>
+    </template>
+    <template #default="{age}">
+      lqnhat not override {{ age }}
+    </template>
+    <template #footer>
+      <div>lqnhat footer</div>
+    </template>
+  </HelloWorld>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot name="header" message="hello"></slot>
+    <slot age="18"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
